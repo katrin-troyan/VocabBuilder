fd67b35cd01f55a0f390165780af1f6d4ec2dfd9
Kateryna Troyan <ket.tro@gmail.com>
2025-12-01 12:15:51 +0200

    add Statistics

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,9 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { mockOwnWords } from "../../data/mockOwnWords";
 
 export default function Statistics() {
+  const toStudy = mockOwnWords.results.length;
+
   return (
-    <View>
-      <Text>Привіт</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        To study: <Text style={styles.number}>{toStudy}</Text>
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    marginBottom: 8,
+  },
+
+  text: {
+    fontFamily: "FixelDisplayMedium",
+    fontSize: 14,
+    color: "rgba(18, 20, 23, 0.5)",
+  },
+
+  number: {
+    fontFamily: "FixelDisplayMedium",
+    fontSize: 20,
+    color: "#121417",
+  },
+});
